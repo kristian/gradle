a672784af93dad7c59eecd78f59a56149f620959
Louis Jacomet <louis@gradle.com>
2019-11-27 20:02:27 +0100

    Write capabilities in available-at variants
    This is required because variant selection will use the local attributes
    and capabilities, adding the real module as a dependency in the current
    implementation.

--- subprojects/publish/src/main/java/org/gradle/api/publish/internal/GradleModuleMetadataWriter.java
+++ subprojects/publish/src/main/java/org/gradle/api/publish/internal/GradleModuleMetadataWriter.java
@@ -278,7 +278,12 @@ public class GradleModuleMetadataWriter {
         jsonWriter.name("name");
         jsonWriter.value(variant.getName());
         writeAttributes(variant.getAttributes(), jsonWriter);
+        writeAvailableAt(coordinates, targetCoordinates, jsonWriter);
+        writeCapabilities("capabilities", variant.getCapabilities(), jsonWriter);
+        jsonWriter.endObject();
+    }
 
+    private void writeAvailableAt(ModuleVersionIdentifier coordinates, ModuleVersionIdentifier targetCoordinates, JsonWriter jsonWriter) throws IOException {
         jsonWriter.name("available-at");
         jsonWriter.beginObject();
 
@@ -291,8 +296,6 @@ public class GradleModuleMetadataWriter {
         jsonWriter.value(targetCoordinates.getName());
         jsonWriter.name("version");
         jsonWriter.value(targetCoordinates.getVersion());
-        jsonWriter.endObject();
-
         jsonWriter.endObject();
     }
 
